fa07bd8e497adb9c907b19fdcb53c6a36bcae219
Gregory Pereverzev <boot762@gmail.com>
2024-12-18 15:52:16 +0300

    feat: change favicon.ico

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,7 +25,7 @@ const getRandomCocktail = useGetRandomCocktail()
 
 <template>
   <app-layout
-    imgUrl="/src/assets/img/bg-1.jpg"
+    imgUrl="/bg-1.jpg"
     :backFunction="clearChoice"
     :isBackButtonVisible="isIngredientChosen"
     :randomCocktailFunction="getRandomCocktail"

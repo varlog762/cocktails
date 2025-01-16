7af090291d96adac8f8789a75091e5b6685ab6ce
Gregory Pereverzev <boot762@gmail.com>
2025-01-16 15:46:31 +0300

    feat: add comments to CocktailThumb component

--- src/components/CocktailThumb.vue
+++ src/components/CocktailThumb.vue
@@ -1,5 +1,15 @@
 <script setup>
+/**
+ * CocktailThumb.vue
+ * This component displays a clickable thumbnail for a cocktail, including:
+ * - An image as the thumbnail background, derived from the cocktail's data.
+ * - The cocktail's name.
+ * - A link to the cocktail's detail page.
+ */
+
+// Defining the props for the component
 const { cocktail } = defineProps({
+  // The `cocktail` object contains details about the cocktail.
   cocktail: {
     type: Object,
     required: true
@@ -8,9 +18,13 @@ const { cocktail } = defineProps({
 </script>
 
 <template>
+  <!-- Root container for the cocktail thumbnail -->
   <div class="root">
+    <!-- Router link to navigate to the cocktail's detail page -->
     <router-link :to="`/cocktails/${cocktail.idDrink}`">
+      <!-- Thumbnail image background -->
       <div class="pic" :style="`background-image: url(${cocktail.strDrinkThumb})`"></div>
+      <!-- Cocktail name displayed below the thumbnail -->
       <div class="name">{{ cocktail.strDrink }}</div>
     </router-link>
   </div>
